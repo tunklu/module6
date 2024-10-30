04749d82ef8ec794c576201d58a6924f99a5f8f8
Denis <vin_ogr@list.ru>
2024-10-30 22:31:11 +0700

    module_6_1.py

--- module_6_1.py
+++ module_6_1.py
@@ -1,3 +1,51 @@
-class Animal
+class Animal:
+    def __init__(self, name):
+        self.name = name
+    alive = True
+    fed = False
 
-class Plant
+
+class Plant:
+    def __init__(self, name):
+        self.name = name
+    edible = False
+    name = ''
+
+class Mammal(Animal):
+    def eat(self, food):
+        if food.edible == True:
+            print (f"{self.name} съел {food.name}")
+            fed = True
+        else:
+            print(f"{self.name} не стал есть {food.name}")
+            fed = False
+
+class Predator(Animal):
+
+    def eat(self, food):
+        if food.edible == True:
+            print (f"{self.name} съел {food.name}")
+            fed = True
+        else:
+            print(f"{self.name} не стал есть {food.name}")
+            fed = False
+
+class Flower(Plant): pass
+
+class Fruit(Plant):
+    edible = True
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
