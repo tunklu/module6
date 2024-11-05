d19f015f4b55703487755d17465a8c09e6ebdd9a
Denis <vin_ogr@list.ru>
2024-11-05 22:42:00 +0700

    module_6_2.py

--- module_6_2.py
+++ module_6_2.py
@@ -1,2 +1,16 @@
-class Vehicle: pass
-class Sedan: pass
+class Vehicle:
+
+    def __init__(self, owner, __model, __engine_power, __color):
+        self.owner = owner
+        self,__model = __model
+        self.__engine_power = __engine_power
+        self.__color = __color
+
+    __COLOR_VARIANTS={'black', 'white', 'silver'}
+
+    def get_model(self):
+        
+
+
+class Sedan(Vehicle):
+    _PASSENGERS_LIMIT = 5
